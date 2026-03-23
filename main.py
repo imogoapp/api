30fcd5f2a0819e5630a449f67f6b3a9413e40563
Josué Juca <josuejuca19@gmail.com>
2026-03-23 17:42:19 -0300

    fix(cors): corrige configuração do CORSMiddleware

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from src.routes.quadracred import router as quadracred_router
 
 app = FastAPI(title="imoGo API", version="2.0.0", description="imoGo App", swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}}, redoc_url=None, docs_url="/api/docs", openapi_url="/api/openapi.json")
 
+# allow_origin_regex=r"https?://([a-z0-9-]+\.)*(imogo\.com\.br|josuejuca\.com|juk\.re)$"
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https?://([a-z0-9-]+\.)*(imogo\.com\.br|josuejuca\.com|juk\.re)$",
+    allow_origins=["*"], # Libera todos os host
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
